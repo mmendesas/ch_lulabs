4a75f4694e4e91e2044281bfc06f2fc0b7fc7399
Marcio Mendes <mmendes.as@gmail.com>
2019-03-27 20:20:31 -0300

    feat: add some tests with react-testing-library

---
+++ src/components/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { render } from 'react-testing-library';
+
+import reducer from '../reducers';
+import App from './App';
+
+// deal with connected components
+const renderWithRedux = (
+  ui,
+  { initialState, store = createStore(reducer, initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    store
+  };
+};
+
+it('can render with redux with defaults', () => {
+  const { getByText } = renderWithRedux(<App />);
+  expect(getByText('Consulta de Endereço')).toBeInTheDocument();
+});
